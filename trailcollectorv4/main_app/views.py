6c43bea859a7a3a472e5e32e38b7ba09eaab8b84
Ali Fasihy <fasihy.ali@gmail.com>
2022-10-24 09:32:54 -0400

    w15-d3

--- trailcollectorv4/main_app/views.py
+++ trailcollectorv4/main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+class Trail: 
+ def __init__(self, name, terrian, description, lenght):
+    self.name = name
+    self.terrian = terrian
+    self.description = description
+    self.lenght =lenght
+trails =[
+Trail('Bruce', 'rocky', 'long long long', 500),
+Trail('Ganaraska', 'foresty', 'asdfasdfasdf asdf',12)
+    ]
+    
+
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def trails_index(request):
+    return render(request,'trails/index.html',{'trails': trails})
